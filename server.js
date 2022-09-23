57055143cd764cbd61f86d78beb7989c6a4f8ea9
Clay McKee <cmckee120993@gmail.com>
2022-09-23 15:28:33 +0200

    changed port to match heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 20300;
 const app = express();
 
 // app.use(express.static('public'));
