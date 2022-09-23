2a7c7d465b8a282cecc29ae8f96ead93645c1de1
Clay McKee <cmckee120993@gmail.com>
2022-09-23 10:48:27 +0200

    note delet function now works on the site; fixed url for delete fetch request

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -37,7 +37,7 @@ const saveNote = (note) =>
   });
 
 const deleteNote = (id) =>
-  fetch(`http://localhost:3001//notes/${id}`, {
+  fetch(`http://localhost:3001/notes/${id}`, {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json',
@@ -78,6 +78,7 @@ const handleNoteDelete = (e) => {
 
   const note = e.target;
   const noteId = JSON.parse(note.parentElement.getAttribute('data-note')).id;
+  console.log(noteId);
 
   if (activeNote.id === noteId) {
     activeNote = {};
